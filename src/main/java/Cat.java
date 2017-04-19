2a0326003e7a3b606eb690fc7522316281e9a46f
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-19 23:51:57 +0600

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,45 @@
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    String address;
+    String color;
+
+    public Cat(String name){
+        this.name = name;
+        this.age = 3;
+        this.weight = 3;
+        this.color = "white";
+    }
+
+    public Cat(String name, int weight, int age){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = "white";
+    }
+
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+        this.weight = 3;
+        this.color = "white";
+    }
+
+    public Cat(int weight, String color){
+        this.age = 3;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat(int weight, String color, String address){
+        this.age = 3;
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
